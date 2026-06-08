d7f6ea45fcfa37b489c3fff9d9919e264f1fe353
NurePohorielovMaksym <maksym.pohorielov@nure.ua>
2026-06-08 19:08:33 +0300

    Delete backend directory

--- backend/src/modules/email-appointment/email-appointment.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import EmailAppointmentController from './email-appointment.controller.js';
-
-const router = express.Router();
-
-router.post('/', EmailAppointmentController.create);
-
-export default router;
